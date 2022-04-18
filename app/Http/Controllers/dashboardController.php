92f22e6823f4ba97c50a47292e94bd2701bcd5d9
ririssilvia <64761595+ririssilvia@users.noreply.github.com>
2022-04-18 22:27:24 +0700

    halaman dashboard setelah login semua user bisa

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -7,6 +7,7 @@ use App\BayiBalita;
 use DB;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class dashboardController extends Controller
 {
@@ -17,9 +18,58 @@ class dashboardController extends Controller
 
     public function dashboard()
     {
-        return view('layout.master');
+        
+        if (Auth::user()->role == 'Kepala PLKB') { // Role Kepala
+            $posyandu = Posyandu::get();
+            $kader = DB::table('users')
+                ->where('role', '=', 'Kader')
+                ->count();
+            $ibubayi = DB::table('users')
+                ->where('role', '=', 'Ibu Bayi')
+                ->count();
+            $bidan = DB::table('users')
+                ->where('role', '=', 'Bidan Desa')
+                ->count();
+            return view('dashboard.operator', compact('posyandu','kader','ibubayi','bidan'));
+        } elseif (Auth::user()->role == 'Operator PLKB') { // Role operator
+                $posyandu = Posyandu::get();
+                $kader = DB::table('users')
+                    ->where('role', '=', 'Kader')
+                    ->count();
+                $ibubayi = DB::table('users')
+                    ->where('role', '=', 'Ibu Bayi')
+                    ->count();
+                $bidan = DB::table('users')
+                    ->where('role', '=', 'Bidan Desa')
+                    ->count();
+                return view('dashboard.operator', compact('posyandu','kader','ibubayi','bidan'));
+            
+        } 
+        elseif (Auth::user()->role == 'Bidan Desa') { // Role Bidan
+            $bayiBalita = BayiBalita::get();
+            $gizibaik = DB::table('timbang')
+                ->count();
+            $bgm = DB::table('timbang')
+                ->where('status_gizi', '=', 'Baik')
+                ->where('status_gizi', '=', 'bgm')
+                ->count();
+            return view('dashboard.kaderBidan', compact('bayiBalita','gizibaik','bgm'));
+        }
+        elseif (Auth::user()->role == 'Kader') { // Role kader
+            $bayiBalita = BayiBalita::get();
+            $gizibaik = DB::table('timbang')
+                ->count();
+            $bgm = DB::table('timbang')
+                ->where('status_gizi', '=', 'Baik')
+                ->where('status_gizi', '=', 'bgm')
+                ->count();
+            return view('dashboard.kaderBidan', compact('bayiBalita','gizibaik','bgm'));
+        }
+        elseif (Auth::user()->role == 'Ibu Bayi') { // Role ibu bayi
+            return view('layout.master');
+        }
     }
-
+    
     public function dashboardOperator()
     {
         $posyandu = Posyandu::get();
